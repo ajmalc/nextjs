87de3511c1647d3b922fa33bedabbc6de6a500a5
Ajmal C <ajmal.chemban@gmail.com>
2020-11-23 17:40:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,15 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
+              <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Ajmal&rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
         </div>
       </main>
 
